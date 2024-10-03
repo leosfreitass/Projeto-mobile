dfa279a749544a64527926840fccd292465336ce
Leo <leo.soares.freitas@gmail.com>
2024-10-03 17:30:49 -0300

    Header.tsx excluido, app voltou a funcionar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,14 +3,11 @@ import Header from "@/components/Header/Header";
 
 export default function RootLayout() {
   return (
-    <>
-      {/* Renderiza o Header globalmente */}
-      <Header />
     
       <Stack>
         <Stack.Screen name="index" />
         <Stack.Screen name="cadastrar" />
       </Stack>
-    </>
+
   );
 }
