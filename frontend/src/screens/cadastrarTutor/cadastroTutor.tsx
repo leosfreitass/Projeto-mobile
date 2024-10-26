dbb4f68b7d7b01158538a1b660fa1a6c734df8b6
Leo <leo.soares.freitas@gmail.com>
2024-10-25 21:19:59 -0300

    Reanimated consertado

--- frontend/src/screens/cadastrarTutor/cadastroTutor.tsx
+++ frontend/src/screens/cadastrarTutor/cadastroTutor.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { View, Text, TextInput, Pressable } from "react-native";
 import { styles } from './cadastroStyle';
-import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import CadastroCachorro from '../cadastrarCachorro/cadastroCachorro';
 
